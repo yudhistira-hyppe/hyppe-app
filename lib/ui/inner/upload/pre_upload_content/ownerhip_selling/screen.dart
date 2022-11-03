571a4bf238bd37ef1030f9dc290e9b6bc63c0674
taslim fuadi <ahmad.taslim07@gmail.com>
2022-11-03 15:53:25 +0700

    fixbug

--- lib/ui/inner/upload/pre_upload_content/ownerhip_selling/screen.dart
+++ lib/ui/inner/upload/pre_upload_content/ownerhip_selling/screen.dart
@@ -245,7 +245,7 @@ class OwnershipSellingScreen extends StatelessWidget {
           child: CustomTextButton(
             onPressed: notifier.certified
                 ? () {
-                    if (!notifier.toSell && notifier.priceController.text == '') {
+                    if (!notifier.toSell && (notifier.priceController.text == '' || notifier.priceController.text == '0')) {
                       Routing().moveBack();
                     } else {
                       final harga = num.parse(notifier.priceController.text.replaceAll(',', '').replaceAll('.', ''));
