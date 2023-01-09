2a5b2e60bc2263d349f221e0dbe292a04ca3dad9
taslim fuadi <ahmad.taslim07@gmail.com>
2023-01-09 17:50:18 +0700

    merge dan fix bug

--- lib/ui/inner/home/screen.dart
+++ lib/ui/inner/home/screen.dart
@@ -1,9 +1,11 @@
+import 'package:hyppe/core/constants/enum.dart';
 import 'package:hyppe/core/constants/shared_preference_keys.dart';
 import 'package:hyppe/core/constants/size_widget.dart';
 import 'package:hyppe/core/extension/log_extension.dart';
 import 'package:hyppe/core/services/shared_preference.dart';
 import 'package:hyppe/initial/hyppe/translate_v2.dart';
 import 'package:hyppe/ui/constant/entities/follow/notifier.dart';
+import 'package:hyppe/ui/constant/entities/report/notifier.dart';
 import 'package:hyppe/ui/inner/home/widget/filter.dart';
 import 'package:hyppe/ui/inner/home/widget/home_app_bar.dart';
 import 'package:hyppe/ui/inner/upload/pre_upload_content/notifier.dart';
