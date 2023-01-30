e032d66b7c6263c02faa729ef60dbf32a743b378
yudhistira-hyppe <yudhistiradewanata@hyppe.id>
2023-01-30 11:25:39 +0700

    update url

--- lib/core/config/url_constants.dart
+++ lib/core/config/url_constants.dart
@@ -20,8 +20,8 @@ class UrlConstants {
 
   /// Production Base url
   // static const String productionBaseApi = "https://prod.hyppe.app";
-  static const String productionBaseApi = "https://prod.hyppe.app";
-  static const String productionUploadBaseApi = "https://upload.hyppe.app/";
+  static const String productionBaseApi = "https://staging.hyppe.app";
+  static const String productionUploadBaseApi = "https://staging.hyppe.app/";
 
   /// Staging v2 Base url
   // static const String stagingBaseApi = "https://prod.hyppe.app";
