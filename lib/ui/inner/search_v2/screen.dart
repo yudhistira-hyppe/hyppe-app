ad82e26fb3dc649085f0efd0b8a0ddd1b9eb3376
Hariyanto Lukman <hariyantosubang@gmail.com>
2023-02-21 16:19:04 +0700

    inprogress layouting

--- lib/ui/inner/search_v2/screen.dart
+++ lib/ui/inner/search_v2/screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hyppe/core/constants/asset_path.dart';
 import 'package:hyppe/core/constants/enum.dart';
 import 'package:hyppe/core/constants/size_config.dart';
 import 'package:hyppe/core/constants/size_widget.dart';
+import 'package:hyppe/core/models/collection/utils/interest/interest_data.dart';
 import 'package:hyppe/core/services/error_service.dart';
 import 'package:hyppe/core/services/route_observer_service.dart';
 import 'package:hyppe/ui/constant/entities/report/notifier.dart';
@@ -9,6 +11,8 @@ import 'package:hyppe/ui/constant/widget/custom_error_widget.dart';
 import 'package:hyppe/ui/constant/widget/custom_search_bar.dart';
 import 'package:hyppe/ui/inner/home/widget/home_app_bar.dart';
 import 'package:hyppe/ui/inner/main/notifier.dart';
+import 'package:hyppe/ui/inner/search_v2/hashtag/screen.dart';
+import 'package:hyppe/ui/inner/search_v2/interest/screen.dart';
 import 'package:hyppe/ui/inner/search_v2/notifier.dart';
 import 'package:hyppe/ui/inner/search_v2/widget/option_bar.dart';
 import 'package:hyppe/ui/inner/search_v2/widget/search_content.dart';
@@ -97,6 +101,57 @@ class _SearchScreenState extends State<SearchScreen> with RouteAware, SingleTick
     'didPush searfc false'.logger();
   }
 
+  final interests = [
+    InterestData(
+      id: '1',
+      interestName: 'Hiburan',
+      interestNameId: '1',
+      icon: '${AssetPath.vectorPath}/hiburan.svg',
+      langID: 'id',
+      cts: '',
+    ),
+    InterestData(
+      id: '2',
+      interestName: 'Gaming',
+      interestNameId: '2',
+      icon: '${AssetPath.vectorPath}/gaming.svg',
+      langID: 'id',
+      cts: '',
+    ),
+    InterestData(
+      id: '3',
+      interestName: 'Film',
+      interestNameId: '3',
+      icon: '${AssetPath.vectorPath}/film.svg',
+      langID: 'id',
+      cts: '',
+    ),
+    InterestData(
+      id: '4',
+      interestName: 'Fashion',
+      interestNameId: '4',
+      icon: '${AssetPath.vectorPath}/fashion.svg',
+      langID: 'id',
+      cts: '',
+    ),
+    InterestData(
+      id: '5',
+      interestName: 'Akun Selebriti',
+      interestNameId: '5',
+      icon: '${AssetPath.vectorPath}/akun_selebriti.svg',
+      langID: 'id',
+      cts: '',
+    ),
+    InterestData(
+      id: '6',
+      interestName: 'Travel',
+      interestNameId: '6',
+      icon: '${AssetPath.vectorPath}/travel.svg',
+      langID: 'id',
+      cts: '',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -115,80 +170,84 @@ class _SearchScreenState extends State<SearchScreen> with RouteAware, SingleTick
           ),
           // endDrawerEnableOpenDragGesture: true,
           // endDrawer: FilterSearchScreen(),
-          body: Column(
-            children: [
-              // const ProcessUploadComponent(topMargin: 0.0),
-              Flexible(
-                child: DefaultTabController(
-                  length: 3,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: CustomSearchBar(
-                          hintText: notifier.language.whatAreYouFindOut,
-                          contentPadding: EdgeInsets.symmetric(vertical: 16 * SizeConfig.scaleDiagonal),
-                          focusNode: notifier.focusNode1,
-                          controller: notifier.searchController1,
-                          // onSubmitted: (v) => notifier.onSearchPost(context, value: v),
-                          // onPressedIcon: () => notifier.onSearchPost(context),
-                          onTap: () => notifier.moveSearchMore(),
-                          // onTap: () => _scaffoldKey.currentState.openEndDrawer(),
-                        ),
-                      ),
-                      // const HashtagScreen(),
-                      TabBar(
-                        controller: _tabController,
-                        // physics: const BouncingScrollPhysics(),
-                        indicatorSize: TabBarIndicatorSize.tab,
-                        labelPadding: const EdgeInsets.symmetric(vertical: 8),
-                        labelColor: Theme.of(context).tabBarTheme.labelColor,
-                        unselectedLabelColor: Theme.of(context).tabBarTheme.unselectedLabelColor,
-                        labelStyle: TextStyle(fontFamily: "Roboto", fontWeight: FontWeight.w400, fontSize: 16 * SizeConfig.scaleDiagonal),
-                        indicator: UnderlineTabIndicator(borderSide: BorderSide(color: Theme.of(context).colorScheme.primary, width: 2.0)),
-                        unselectedLabelStyle: TextStyle(fontFamily: "Roboto", fontWeight: FontWeight.w400, fontSize: 16 * SizeConfig.scaleDiagonal),
-                        tabs: const [
-                          OptionBar(title: "Vid", icon: "vid", pageIndex: 0),
-                          OptionBar(title: "Diary", icon: "diary", pageIndex: 1),
-                          OptionBar(title: "Pic", icon: "pic", pageIndex: 2),
-                          // CustomElevatedButton(
-                          //     child: CustomTextWidget(
-                          //       textToDisplay: context.select((SearchNotifier value) => value.language.filter),
-                          //       textStyle: Theme.of(context).textTheme.button,
-                          //     ),
-                          //     width: 50,
-                          //     height: 20 * SizeConfig.scaleDiagonal,
-                          //     function: () {},
-                          // ),
-                        ],
-                        onTap: (index) => notifier.pageIndex = index,
-                      ),
-                      context.read<ErrorService>().isInitialError(error, notifier.allContents)
-                          ? Center(
-                              child: SizedBox(
-                                height: 198,
-                                child: CustomErrorWidget(
-                                  errorType: ErrorType.getPost,
-                                  function: () => notifier.onInitialSearch(context),
-                                ),
-                              ),
-                            )
-                          : Flexible(
-                              child: TabBarView(
-                                physics: const NeverScrollableScrollPhysics(),
-                                controller: _tabController,
-                                children: [
-                                  SearchContent(featureType: FeatureType.vid, index: 0),
-                                  SearchContent(featureType: FeatureType.diary, index: 1),
-                                  SearchContent(featureType: FeatureType.pic, index: 2),
-                                ],
-                              ),
-                            ),
-                    ],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: CustomSearchBar(
+                    hintText: notifier.language.whatAreYouFindOut,
+                    contentPadding: EdgeInsets.symmetric(vertical: 16 * SizeConfig.scaleDiagonal),
+                    focusNode: notifier.focusNode1,
+                    controller: notifier.searchController1,
+                    // onSubmitted: (v) => notifier.onSearchPost(context, value: v),
+                    // onPressedIcon: () => notifier.onSearchPost(context),
+                    onTap: () => notifier.moveSearchMore(),
+                    // onTap: () => _scaffoldKey.currentState.openEndDrawer(),
                   ),
                 ),
-              ),
-            ],
+                const HashtagScreen(),
+                InterestScreen(data: interests,),
+                // const ProcessUploadComponent(topMargin: 0.0),
+                // Flexible(
+                //   child: DefaultTabController(
+                //     length: 3,
+                //     child: Column(
+                //       children: [
+                //
+                //         TabBar(
+                //           controller: _tabController,
+                //           // physics: const BouncingScrollPhysics(),
+                //           indicatorSize: TabBarIndicatorSize.tab,
+                //           labelPadding: const EdgeInsets.symmetric(vertical: 8),
+                //           labelColor: Theme.of(context).tabBarTheme.labelColor,
+                //           unselectedLabelColor: Theme.of(context).tabBarTheme.unselectedLabelColor,
+                //           labelStyle: TextStyle(fontFamily: "Roboto", fontWeight: FontWeight.w400, fontSize: 16 * SizeConfig.scaleDiagonal),
+                //           indicator: UnderlineTabIndicator(borderSide: BorderSide(color: Theme.of(context).colorScheme.primary, width: 2.0)),
+                //           unselectedLabelStyle: TextStyle(fontFamily: "Roboto", fontWeight: FontWeight.w400, fontSize: 16 * SizeConfig.scaleDiagonal),
+                //           tabs: const [
+                //             OptionBar(title: "Vid", icon: "vid", pageIndex: 0),
+                //             OptionBar(title: "Diary", icon: "diary", pageIndex: 1),
+                //             OptionBar(title: "Pic", icon: "pic", pageIndex: 2),
+                //             // CustomElevatedButton(
+                //             //     child: CustomTextWidget(
+                //             //       textToDisplay: context.select((SearchNotifier value) => value.language.filter),
+                //             //       textStyle: Theme.of(context).textTheme.button,
+                //             //     ),
+                //             //     width: 50,
+                //             //     height: 20 * SizeConfig.scaleDiagonal,
+                //             //     function: () {},
+                //             // ),
+                //           ],
+                //           onTap: (index) => notifier.pageIndex = index,
+                //         ),
+                //         context.read<ErrorService>().isInitialError(error, notifier.allContents)
+                //             ? Center(
+                //                 child: SizedBox(
+                //                   height: 198,
+                //                   child: CustomErrorWidget(
+                //                     errorType: ErrorType.getPost,
+                //                     function: () => notifier.onInitialSearch(context),
+                //                   ),
+                //                 ),
+                //               )
+                //             : Flexible(
+                //                 child: TabBarView(
+                //                   physics: const NeverScrollableScrollPhysics(),
+                //                   controller: _tabController,
+                //                   children: [
+                //                     SearchContent(featureType: FeatureType.vid, index: 0),
+                //                     SearchContent(featureType: FeatureType.diary, index: 1),
+                //                     SearchContent(featureType: FeatureType.pic, index: 2),
+                //                   ],
+                //                 ),
+                //               ),
+                //       ],
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
           ),
         ),
       ),
