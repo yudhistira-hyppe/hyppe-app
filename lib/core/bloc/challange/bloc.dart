a1d69791f2b81126d55618ddce0407cff421d491
Hariyanto Lukman <hariyantosubang@gmail.com>
2023-10-06 13:14:35 +0700

    add check challange join in create a post

--- lib/core/bloc/challange/bloc.dart
+++ lib/core/bloc/challange/bloc.dart
@@ -5,10 +5,13 @@ import 'package:hyppe/core/constants/enum.dart';
 import 'package:hyppe/core/constants/status_code.dart';
 import 'package:hyppe/core/models/collection/chalange/banner_request.dart';
 import 'package:hyppe/core/response/generic_response.dart';
+import 'package:hyppe/core/services/shared_preference.dart';
 import 'package:hyppe/ui/constant/overlay/bottom_sheet/show_bottom_sheet.dart';
 import 'package:hyppe/ux/routing.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+
+import '../../constants/shared_preference_keys.dart';
 
 class ChallangeBloc {
   ChallangeFetch _challangeFetch = ChallangeFetch(ChallengeState.init);
@@ -73,5 +76,27 @@ class ChallangeBloc {
       host: UrlConstants.getBanners,
       methodType: MethodType.post,
     );
+  }
+
+  Future checkChallengeStatus(BuildContext context) async {
+    final userId = SharedPreference().readStorage(SpKeys.userID);
+    setChallangeFetch(ChallangeFetch(ChallengeState.loading));
+    await Repos().reposPost(context, (onResult){
+      if ((onResult.statusCode ?? 300) > HTTP_CODE) {
+        setChallangeFetch(ChallangeFetch(ChallengeState.getPostError, data: onResult.data));
+      } else {
+        setChallangeFetch(ChallangeFetch(ChallengeState.getPostSuccess, data: GenericResponse.fromJson(onResult.data).responseData));
+      }
+    }, (errorData) {
+      ShowBottomSheet.onInternalServerError(context);
+      setChallangeFetch(ChallangeFetch(ChallengeState.getPostError));
+      Dio().close(force: true);
+    }, host: UrlConstants.checkChallengeStatus,
+        data: {
+          'idUser' : userId
+        },
+        withAlertMessage: false,
+        methodType: MethodType.post,
+        withCheckConnection: false);
   }
 }
