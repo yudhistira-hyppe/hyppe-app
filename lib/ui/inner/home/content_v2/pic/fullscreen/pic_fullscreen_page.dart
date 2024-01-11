b6f9a8916537fcf5682e33c8d91e1631aa181a59
Hyppe Development <hyppedevelopment@Hyppes-MacBook-Pro-3.local>
2024-01-11 15:46:02 +0700

    Tested

--- lib/ui/inner/home/content_v2/pic/fullscreen/pic_fullscreen_page.dart
+++ lib/ui/inner/home/content_v2/pic/fullscreen/pic_fullscreen_page.dart
@@ -1,9 +1,11 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hyppe/core/arguments/pic_fullscreen_argument.dart';
 import 'package:hyppe/core/constants/size_config.dart';
 import 'package:hyppe/core/models/collection/posts/content_v2/content_data.dart';
+import 'package:provider/provider.dart';
 
 import '../screen.dart';
 
@@ -32,36 +34,39 @@ class _PicFullscreenPageState extends State<PicFullscreenPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          Center(
-            child: ImageSize(
-              onChange: (Size size) {
-                if ((picData?.imageHeightTemp ?? 0) == 0) {
-                  setState(() {
-                    picData?.imageHeightTemp = size.height;
-                  });
-                }
-              },
-              child: picData?.reportedStatus == 'BLURRED'
-                  ? ImageFiltered(
-                      imageFilter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-                      child: Image(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Stack(
+          children: [
+            Center(
+              child: ImageSize(
+                onChange: (Size size) {
+                  if ((picData?.imageHeightTemp ?? 0) == 0) {
+                    setState(() {
+                      picData?.imageHeightTemp = size.height;
+                    });
+                  }
+                },
+                child: picData?.reportedStatus == 'BLURRED'
+                    ? ImageFiltered(
+                        imageFilter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                        child: Image(
+                          image: imageProvider,
+                          fit: BoxFit.fitHeight,
+                          width: SizeConfig.screenWidth,
+                          // height: picData?.imageHeightTemp == 0 ? null : picData?.imageHeightTemp,
+                        ),
+                      )
+                    : Image(
                         image: imageProvider,
                         fit: BoxFit.fitHeight,
                         width: SizeConfig.screenWidth,
-                        // height: picData?.imageHeightTemp == 0 ? null : picData?.imageHeightTemp,
+                        // height: picData?.imageHeightTemp == 0 || (picData?.imageHeightTemp ?? 0) <= 100 ? null : picData?.imageHeightTemp,
                       ),
-                    )
-                  : Image(
-                      image: imageProvider,
-                      fit: BoxFit.fitHeight,
-                      width: SizeConfig.screenWidth,
-                      // height: picData?.imageHeightTemp == 0 || (picData?.imageHeightTemp ?? 0) <= 100 ? null : picData?.imageHeightTemp,
-                    ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
