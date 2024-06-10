1297f634e3a3ee3028c6133f93b6a13acac54eb9
tomo <htmrhdt@gmail.com>
2024-06-10 11:50:31 +0700

    set new param to viewPostUserBloc

--- lib/core/bloc/view/bloc.dart
+++ lib/core/bloc/view/bloc.dart
@@ -32,11 +32,7 @@ class ViewBloc {
     required bool active,
   }) async {
     final email = SharedPreference().readStorage(SpKeys.email);
-    final Map<String, dynamic> _data = {
-      "eventType": "VIEW",
-      "postID": postId,
-      "receiverParty": emailOwner,
-    };
+    final Map<String, dynamic> _data = {"eventType": "VIEW", "postID": postId, "receiverParty": emailOwner, "userView": userView, "userLike": userLike, "saleAmount": saleAmount, "createdAt": createdAt, "mediaSource": mediaSource, "description": description, "active": active};
 
     String challangedata = SharedPreference().readStorage(SpKeys.challangeData) ?? '';
     if (challangedata != '') {
